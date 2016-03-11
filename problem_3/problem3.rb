c344c3198eb14e018e5748680b7ff5d80ae39d98
Adam Adair <admaadair@outlook.com>
2016-03-11 17:39:16 -0600

    problem 3

---
+++ problem_3/problem3.rb
@@ -0,0 +1,9 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+#
+# What is the largest prime factor of the number 600851475143 ?
+
+# A prime number (or a prime) is a natural number greater than 1 that has no 
+# positive divisors other than 1 and itself. In number theory, the prime 
+# factors of a positive integer are the prime numbers that divide that integer 
+# exactly.
+
