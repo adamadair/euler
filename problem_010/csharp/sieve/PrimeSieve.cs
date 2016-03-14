d5bdef6067d463a72ec968842006ee0dde6ae79a
Adam Adair <adamadair@outlook.com>
2016-03-14 00:18:02 -0500

    Added sieve in csharp

---
+++ problem_010/csharp/sieve/PrimeSieve.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace sieve
+{
+	/// <summary>
+	/// The Prime sieve is an implementation of the sieve of Eratosthenes. The underlying
+	/// data structure only stores values pertaining to odd numbers, so it is generally 
+	/// half the amount of memory used for the upper bounds of the sieve. However, it is
+	/// generally a good idea to keep the sieve size under 1000000000 or so. I have not
+	/// tested it for sieves greater than a billion.
+	/// </summary>
+	public class PrimeSieve
+	{
+		private bool[] sieve;
+		private int limit;
+		private long sum;
+		public PrimeSieve (int size)
+		{
+			limit = size;
+			initSieve ();
+		}
+
+		private void initSieve()
+		{
+			int sieveBound = (limit - 1) / 2; // last index of sieve
+			sieve = new bool[sieveBound + 1]; // auto initialized to false
+
+			int crossLimit = System.Convert.ToInt32( (Math.Floor (Math.Sqrt (limit))-1)/2);
+
+			for (int i = 1; i <= crossLimit; ++i) 
+			{
+				if (!sieve [i]) {
+					for (int j = 2 * i * (i + 1); j <= sieveBound; j += 2 * i + 1) {
+						sieve [j] = true;
+					}
+				}
+			}
+			sum = 2;
+			for (int i = 1; i <= sieveBound; ++i) {
+				if (!sieve [i])
+					sum += (2 * i + 1);
+			}
+		}
+
+		/// <summary>
+		/// Returns the sum of all primes in the sieve. This really only
+		/// exists to satisfy the Euler Project problem 10 requirements.
+		/// </summary>
+		/// <value>The sum of primes.</value>
+		public long SumOfPrimes
+		{
+			get { return sum; }
+		}
+	}
+}
+
---
+++ problem_010/csharp/sieve/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace sieve
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			PrimeSieve sieve = new PrimeSieve (2000000);
+			Console.WriteLine ("sum = {0}",sieve.SumOfPrimes);
+		}
+	}
+}
