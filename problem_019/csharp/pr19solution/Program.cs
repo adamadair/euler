3632b1d18b63626db2353d1b0d67c02b87bf2300
Adam Adair <admaadair@outlook.com>
2016-03-18 16:32:04 -0500

    Problem 19 completed in C#

---
+++ problem_019/csharp/pr19solution/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace pr19solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            for(int year=1901; year <=2000; ++year)
+            {
+                for(int month = 1; month <= 12; ++month)
+                {
+                    if (new DateTime(year, month, 1).DayOfWeek == DayOfWeek.Sunday)
+                        ++count;
+                }
+            }
+            Console.WriteLine(count.ToString());
+            Console.ReadLine();
+        }
+    }
+}
