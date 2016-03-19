322bfe827682eef7a0e55bf2b5a7a83e239299a7
adamadair@outlook.com <adamadair@outlook.com>
2016-03-19 15:47:39 -0500

    complete p24

--- problem_024/problem24.md
+++ problem_024/problem24.md
@@ -5,3 +5,20 @@ A permutation is an ordered arrangement of objects. For example, 3124 is one pos
 012   021   102   120   201   210
 
 What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+
+# Solution Notes
+
+This one was a fun one, and it actually required some thought. The first reaction was to brute force it and
+just create all permutations and then sort the array, then pick the millionth item in the array, but that would
+create twice as many permutations as necessary. So a little searching on the internet turned up that this 
+problem had been solved as early as the 14th century in India. I took the algorithm described on [Wikipedia](https://en.wikipedia.org/wiki/Permutation#Generation_in_lexicographic_order) and
+turned it into C#. 
+
+The following algorithm generates the next permutation lexicographically after a given permutation. It changes the given permutation in-place.
+
+1. Find the largest index `k` such that `a[k] < a[k + 1]`. If no such index exists, the permutation is the last permutation.
+2. Find the largest index `l` greater than k such that `a[k] < a[l]`.
+3. Swap the value of `a[k]` with that of `a[l]`.
+4. Reverse the sequence from `a[k + 1]` up to and including the final element `a[n]`.
+
+The only difference to the algorithm is that I've added a counter so that it stops at the millionth.
--- problem_024/solution24/solution24/Program.cs
+++ problem_024/solution24/solution24/Program.cs
@@ -10,27 +10,88 @@ namespace solution24
 	{
 		public static void Main (string[] args)
 		{
-			int max = 10;
+			//int max = 10;
 			string[] digits = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-			var els = digits.Combinations (max);
-			foreach (var e in els) {
-				Console.WriteLine (e);
-			}
+
+			string[] lp = LexoPerm (digits, 1000000-1);
+
+			PrintArray (lp);
 			//Console.WriteLine (digits.Combinations (max).ElementAt (max - 1).ToString ());
 			Console.WriteLine ();
 				
 		}
 
-
-	}
-
-	public static class Combo
-	{
-		public static IEnumerable<IEnumerable<T>> Combinations<T>(this IEnumerable<T> elements, int k)
+		/// <summary>
+		/// Returns the nth lexographic permutation of the array.
+		/// 
+		/// 1. Find the largest index `k` such that `a[k] < a[k + 1]`. 
+		/// If no such index exists, the permutation is the last permutation.
+		/// 
+		/// 2. Find the largest index `l` greater than k such that `a[k] < a[l]`.
+		/// 
+		/// 3. Swap the value of `a[k]` with that of `a[l]`.
+		/// 
+		/// 4. Reverse the sequence from `a[k + 1]` up to and including the final element `a[n]`.
+		/// </summary>
+		/// <returns>The kth permutation. If there are not k perumutations the last 
+		/// permutation is returned.</returns>
+		/// <param name="sArrray">the array</param>
+		/// <param name="n">which permutation to return</param>
+		private static string [] LexoPerm(string [] sArray, int n)
 		{
-			return k == 0 ? new[] { new T[0] } :
-			elements.SelectMany((e, i) =>
-				elements.Skip(i + 1).Combinations(k - 1).Select(c => (new[] {e}).Concat(c)));
+			int length = sArray.Length;
+			var tmpArray = new string[length];
+			Array.Copy (sArray, tmpArray, length);
+			for (int i = 0; i < n; ++i) {
+
+				// 1. Find the largest index `k` such that `a[k] < a[k + 1]`. 
+				// If no such index exists, the permutation is the last permutation.
+				int k = tmpArray.Length - 2; //point k to the last element in the array
+				while (k >= 0 && tmpArray[k].CompareTo(tmpArray[k+1])>=0){ // >= tmpArray [k + 1]) {
+					--k;
+				}
+				if (k < 0)
+					return tmpArray;
+
+				// 2. Find the largest index `l` greater than k such that `a[k] < a[l]`.
+				int l = length-1;
+				while(tmpArray[l].CompareTo(tmpArray[k])<=0){
+					--l;
+				}
+
+				// 3. Swap the value of `a[k]` with that of `a[l]`.
+				Swap(tmpArray,k,l);
+
+				// 4. Reverse the sequence from `a[k + 1]` up to and including the final element `a[n]`.
+				Reverse(tmpArray, k+1, length-1);
+				//PrintArray (tmpArray);
+			}
+			return tmpArray;
+		}
+
+		private static void Swap(string []sArray, int k, int l)
+		{
+			string tmp = sArray [k];
+			sArray [k] = sArray [l];
+			sArray [l] = tmp;
+		}
+
+		private static void PrintArray(string []sa){
+			foreach (var d in sa)
+				Console.Write (d);
+			Console.WriteLine ();
+
+		}
+
+		private static void Reverse(string []sArray, int start, int end)
+		{
+			if (start < 0 || start >= sArray.Length || end < 0 || end >= sArray.Length)
+				return;
+			while (start < end) {
+				Swap (sArray, start, end);
+				++start;
+				--end;
+			}
 		}
 	}
 }
