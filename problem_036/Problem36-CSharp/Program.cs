466443353764853923bd0de7f511de11e9c96d39
Adam Adair <admaadair@outlook.com>
2016-03-23 10:39:57 -0500

    C# solution based on overview.

---
+++ problem_036/Problem36-CSharp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using EulerLib;
+
+namespace Problem36_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int limit = 1000000;
+            int sum = 0;
+            int i = 1;
+            int p = MakePalindrome(i, 2, true);
+            while (p < limit)
+            {
+                if (Number.IsPalindrome(p, 10))
+                    sum += p;
+                ++i;
+                p = MakePalindrome(i, 2, true);
+            }
+            i = 1;
+            p = MakePalindrome(i, 2, false);
+            while (p < limit)
+            {
+                if (Number.IsPalindrome(p, 10))
+                    sum += p;
+                ++i;
+                p = MakePalindrome(i, 2, false);
+            }
+            Console.WriteLine(sum.ToString());
+            Console.ReadLine();
+        }
+
+        static int MakePalindrome(int n, int b, bool oddLength)
+        {
+            int res = n;
+            if (oddLength)
+                n /= b;
+            while (n > 0)
+            {
+                res = b * res + n % b;
+                n /= b;
+            }
+            return res;
+        }
+    }
+}
