57ecbe0916cebfbee2e5c9a0cdb4748a81e322c1
Adam Adair <admaadair@outlook.com>
2016-03-24 14:39:42 -0500

    Goldbach's other conjecture

---
+++ problem_046/csharp/Problem46-VS/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EulerLib;
+
+namespace Problem46_VS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Building double square list");
+            List<long> DoubleSquares = new List<long>();
+            for(long i = 1; i< 1000000; ++i)
+            {
+                DoubleSquares.Add(2 * (i * i));
+            }
+            long lastLong = 7L;
+            bool conjecture = true;
+            long[] ds = DoubleSquares.ToArray();
+            DoubleSquares.Clear(); DoubleSquares = null;
+            Console.WriteLine("Searching...");
+            do
+            {
+                lastLong += 2;
+                conjecture = false; // Assume false, prove true
+                if (!lastLong.IsPrime())
+                {
+                    // lastLong is composite
+                    int index = 0;
+                    do
+                    {
+                        if ((lastLong - ds[index]).IsPrime())
+                        {
+                            conjecture = true;
+                        }
+                        ++index;
+                    } while (index < ds.Length && !conjecture);
+                }
+                else
+                {
+                    conjecture = true;
+                }
+            } while (conjecture);
+            Console.WriteLine(lastLong.ToString());
+            Console.ReadLine();
+        }
+    }
+}
--- problem_046/problem46.md
+++ problem_046/problem46.md
@@ -15,3 +15,7 @@ It turns out that the conjecture was false.
 What is the smallest odd composite that cannot be written as the sum of a prime and twice a square?
 
 # Solution Notes
+Wrote this one in C#. I was surprised by the answer. It's almost like Goldback didn't 
+even try to check his conjecture. I expected this to be large number so I extended the
+prime checking code to include long integer types, and I think I could have completed 
+this with ordinary 32bit integers. 
