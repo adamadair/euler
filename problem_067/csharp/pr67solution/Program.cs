6c8286da89b2d54e7b95700c29fc4e15d7220196
Adam Adair <adamadair@outlook.com>
2016-04-01 22:55:05 -0500

    Maximum path sum II

---
+++ problem_067/csharp/pr67solution/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr18solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new Triangle("p067_triangle.txt");
+            int max = t.GetMaxTotal();
+            Console.WriteLine("Max Value = {0}", max);
+            Console.ReadLine();
+        }
+    }
+}
