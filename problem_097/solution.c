9f3fb0b30b5e32a36cb6b44e9d58f2aa3b369d13
adamadair@outlook.com <adamadair@outlook.com>
2016-04-02 10:35:25 -0500

    Large non-Mersenne prime

--- problem_097/solution.c
+++ problem_097/solution.c
@@ -2,14 +2,14 @@
 int
 main(){
   int i = 0;
-  long res = 1;
+  long result = 1;
   for (i = 0; i < 7830457; i++)
   {
-    res <<= 1;
-    res %= 10000000000;
+    result <<= 1;
+    result %= 10000000000;
   }
-  res *= 28433;
-  res %= 10000000000;
-  res += 1;
-  printf("%ld\n",res);
+  result *= 28433;
+  result %= 10000000000;
+  result += 1;
+  printf("%ld\n",result);
 }
