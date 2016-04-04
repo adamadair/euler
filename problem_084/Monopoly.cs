ebf48d54e7ec61470d808063589c59f760ec0739
adamadair@outlook.com <adamadair@outlook.com>
2016-04-04 07:47:08 -0500

    Monopoly

---
+++ problem_084/Monopoly.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Monopoly
+{
+    public class Monopoly
+    {
+        
+        public Monopoly()
+        {
+        }
+
+
+    }
+
+    public enum SquareType
+    {
+        Normal,
+        Jail,
+        CommunityChest,
+        Chance,
+        Railway,
+        GoToJail
+    }
+
+    public class Square
+    {
+        public Square(string name, SquareType type, int visits)
+        {
+            Name = name;
+            Type = type;
+            Visits = visits;
+        }
+        public SquareType Type { get; set; }
+        public string Name { get; set; }
+        public int Visits { get; set; }
+    }
+
+
+}
+
