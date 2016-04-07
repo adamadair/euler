e5670608522d3f7b2b6d2ece2ade848371ad7347
Adam Adair <adamadair@outlook.com>
2016-04-06 22:46:40 -0500

    113

---
+++ problem_113/solution.rb
@@ -0,0 +1,14 @@
+def factorial(n)
+  return 1 if n==0
+  n*factorial(n-1)
+end
+
+def calc(n,r)
+  factorial(n)/factorial(n-r)/factorial(r)
+end
+
+def nN(n)
+  calc(n+10,10)+c(n+9,9)-9*n-(n+1)-1
+end
+
+puts nN(100)
