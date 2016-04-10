ebb296d29328f6ee3826f4833c684e0003e20075
adamadair@outlook.com <adamadair@outlook.com>
2016-04-10 10:32:58 -0500

    Matrix Sum

--- problem_345/awa_matrix.rb
+++ problem_345/awa_matrix.rb
@@ -10,14 +10,35 @@ class AwaMatrix
 
   def sum
     (0..@rows).to_a.each{
-      @max_sum << array.new((1<<@cols), 0)
+      @max_sum << Array.new((1<<@cols), 0)
     }
-    0
+    puts @max_sum.length
+    find_max_sum(0,(1<<@cols)-1)
   end
 
   private
-  # https://github.com/nayuki/Project-Euler-solutions/blob/master/java/p345.java
-   # private int findMaximumSum(int startRow, int setOfCols) {
+  def find_max_sum(start_row, set_of_cols)
+    return 0 if start_row==@rows
+    puts "#{start_row} - #{set_of_cols}"
+    if @max_sum[start_row][set_of_cols]==0
+      max = 0
+      col = 0
+      while (1<<col)<=set_of_cols
+        max = max_i(@matrix[start_row][col]+find_max_sum(start_row+1, set_of_cols ^ (1<<col)), max) if set_of_cols & (1 << col) != 0
+        @max_sum[start_row][set_of_cols]=max
+        col += 1
+      end
+    end
+    @max_sum[start_row][set_of_cols]
+  end
+
+  def max_i(a,b)
+    return a if a > b
+    b
+  end
+
+    # https://github.com/nayuki/Project-Euler-solutions/blob/master/java/p345.java
+    # private int findMaximumSum(int startRow, int setOfCols) {
     # if (startRow == ROWS) {
     #     if (Integer.bitCount(setOfCols) != COLUMNS - ROWS)
     #       throw new AssertionError();
