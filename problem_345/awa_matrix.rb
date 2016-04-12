d912c9961bd443ed86b795d16c210038a888f869
adamadair@outlook.com <adamadair@outlook.com>
2016-04-11 21:26:00 -0500

    Matrix Sum

--- problem_345/awa_matrix.rb
+++ problem_345/awa_matrix.rb
@@ -36,25 +36,4 @@ class AwaMatrix
     return a if a > b
     b
   end
-
-    # https://github.com/nayuki/Project-Euler-solutions/blob/master/java/p345.java
-    # private int findMaximumSum(int startRow, int setOfCols) {
-    # if (startRow == ROWS) {
-    #     if (Integer.bitCount(setOfCols) != COLUMNS - ROWS)
-    #       throw new AssertionError();
-    #       return 0;
-    #       }
-    #
-    #       if (maxSum[startRow][setOfCols] == 0) {
-    #           int max = 0;
-    #       for (int col = 0; (1 << col) <= setOfCols; col++) {
-    #           if ((setOfCols & (1 << col)) != 0)
-    #             max = Math.max(MATRIX[startRow][col] + findMaximumSum(startRow + 1, setOfCols ^ (1 << col)), max);
-    #             }
-    #             maxSum[startRow][setOfCols] = max;
-    #             }
-    #             return maxSum[startRow][setOfCols];
-    #             }
-
-
 end
